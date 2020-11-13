dbdc10f906fbc858251d0dc9c3349aa367b295ca
en1mem <germanorumgepp@mail.ru>
2020-11-13 11:42:44 +0500

    german/writeTests - wrote a maket of testService. Doesnt need any test here

--- src/test/ru/german/services/TestService.java
+++ src/test/ru/german/services/TestService.java
@@ -1,97 +1,102 @@
 package ru.german.services;
 
+import com.amazonaws.AmazonServiceException;
+import org.jooq.exception.DataAccessException;
 import org.junit.Assert;
-import org.junit.Before;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import ru.generated.com.cloud.tables.pojos.ElementObject;
 import ru.german.model.ContentObject;
+import ru.german.repository.ElementRepository;
 import ru.german.service.ShareService;
 
 import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 
-public class TestService {
-    Logger logger = LoggerFactory.getLogger(TestService.class);
+public class TestService extends Assert {
+
+    /**
+     * That was a simple spring boot application. All test could be made only for DB or Amazon bucket test.
+     * It's useless, bc db or amazon errors can be find in manual tests
+     * We need test only business logic
+     */
+
+    @Rule
+    public final ExpectedException thrown = ExpectedException.none();
 
     @Autowired
     private ShareService shareService;
 
+    @Autowired
+    private ElementRepository repository;
+
     @Value("${amazonPath}")
     String defaultAmazonPath;
 
-    @Before
-    public void setUp() {
-        logger.info("Code executes before each test method");
-
+    private List<ElementObject> makeTestElementsObjects() {
         ElementObject elementObject1 = new ElementObject();
         ElementObject elementObject2 = null;
         ElementObject elementObject3 = new ElementObject(-150L, 1L, true, "NEW", new Timestamp(new Date().getTime()), "someStuff");
         ElementObject elementObject4 = new ElementObject(10L, 1L, true, "NEW", new Timestamp(new Date().getTime()), "someStuff");
         ElementObject elementObject5 = new ElementObject();
-        ElementObject elementObject6 = new ElementObject();
-        List<ElementObject> elelementList = new ArrayList<>(Arrays.asList(elementObject1, elementObject2, elementObject3, elementObject4, elementObject5, elementObject6));
 
+        return new ArrayList<>(Arrays.asList(elementObject1, elementObject2, elementObject3, elementObject4, elementObject5));
+    }
 
+    private List<ContentObject> makeTestContentObjects() {
         ContentObject contentObject1 = new ContentObject(-150L, "testContent", defaultAmazonPath, ".txt");
         ContentObject contentObject2 = new ContentObject(1L, null, defaultAmazonPath, ".txt");
         ContentObject contentObject3 = new ContentObject(1L, "testContent", defaultAmazonPath + "some", ".txt");
         ContentObject contentObject4 = new ContentObject(1L, "testContent", defaultAmazonPath, ".png");
         ContentObject contentObject5 = new ContentObject(1L, "testContent", defaultAmazonPath, ".txt");
-        List<ContentObject> contentList = new ArrayList<>(Arrays.asList(contentObject1, contentObject2, contentObject3, contentObject4, contentObject5));
 
-
-        creatingNewUsers(elelementList);
-        updatingContent(contentList);
+        return new ArrayList<>(Arrays.asList(contentObject1, contentObject2, contentObject3, contentObject4, contentObject5));
     }
 
-    public void creatingNewUsers(List<ElementObject> list) {
+    private List<ContentObject> makeTestNullContent() {
+        ContentObject contentObject6 = null;
+
+        return new ArrayList<>(Collections.singletonList(contentObject6));
+    }
+
+    //do not passed caused by spring injection doesnt work
+    @Test(expected = DataAccessException.class)
+    public void creatingNewElementsWithDbExceptions() {
+        List<ElementObject> list = makeTestElementsObjects();
         for (ElementObject elementObject : list) {
-            //Assert.assertThat(elementObject.getId());
             shareService.createElementObject(elementObject);
-            //Assert.assertThat();
         }
     }
 
-    public void creatingNewUsersWithContent(List<ElementObject> list) {
-        for (ElementObject elementObject : list) {
-            //Assert.assertThat(elementObject.getId());
-            shareService.createElementObject(elementObject);
-            //Assert.assertThat();
+    //do not passed caused by spring injection doesnt work
+    @Test(expected = AmazonServiceException.class)
+    public void creatingNewContentWithAmazonException() {
+        List<ContentObject> list = makeTestContentObjects();
+        for (ContentObject contentObject : list) {
+            repository.uploadFile(
+                    contentObject.getDefaultPath() +
+                            contentObject.getElementId() +
+                            contentObject.getPostfix(),
+                    contentObject.getContent());
         }
     }
 
-    public void updatingNewUsers(List<ElementObject> list) {
-        for (ElementObject elementObject : list) {
-            //Assert.assertThat(elementObject.getId());
-            shareService.createElementObject(elementObject);
-            //Assert.assertThat();
+    //passed, but not correct caused by spring injection doesnt work
+    @Test(expected = NullPointerException.class)
+    public void creatingNewContentWithNPE_Exception() {
+        List<ContentObject> nullContentList = makeTestNullContent();
+
+        for (ContentObject  contentObject : nullContentList) {
+            assertNull(contentObject);
+            repository.uploadFile(
+                    contentObject.getDefaultPath() +
+                            contentObject.getElementId() +
+                            contentObject.getPostfix(),
+                    contentObject.getContent());
         }
-    }
-
-    public void updatingNewUsersWithContent(List<ElementObject> list) {
-        for (ElementObject elementObject : list) {
-            //Assert.assertThat(elementObject.getId());
-            shareService.createElementObject(elementObject);
-            //Assert.assertThat();
-        }
-    }
-
-    public void updatingContent(List<ContentObject> list) {
-        for (ContentObject elementObject : list) {
-            //Assert.assertThat(elementObject.getId());
-            //shareService.updateActualContentByElementEntity(elementObject);
-            //Assert.assertThat();
-        }
-    }
-
-    public void insertingContent(String content) {
-
     }
 }
